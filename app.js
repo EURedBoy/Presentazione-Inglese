70261054f1a252a6a213e46fddad17adcb95b696
alex <ALEX.AROSIO@issgreppi.it>
2023-03-30 23:14:11 +0200

    Half work

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
-        console.log(entry)
         if (entry.isIntersecting) entry.target.classList.add('show');
         else entry.target.classList.remove('show');
     })
 })
 
-
+let previous = null;
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach(el => observer.observe(el));
+
+const close = document.querySelector('.close');
+
+close.addEventListener('click', event => {
+    event.target.parentNode.classList.toggle('open');
+});
+
+function openPopup(className) {
+    document.querySelector('.' + className).classList.toggle('open');
+}
+
+function openPar(className) {
+    if (previous != null) {
+        previous.classList.toggle('vedi')
+    }
+    var current = document.querySelector('.' + className);
+    current.classList.toggle('vedi');
+    previous = current;
+}
+
